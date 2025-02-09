97b610da15bec90ab7c0fd37ac0aef86da88598f
Kevin T <126206698+kevintran120@users.noreply.github.com>
2025-02-09 06:29:09 +0000

    finished video

---
+++ 2-Loops/hogwarts.py
@@ -0,0 +1,20 @@
+students = [
+    {"name": "Hermione",
+    "house": "Gryffindor",
+    "patronus": "Otter"},
+
+    {"name": "Harry",
+    "house": "Gryffindor",
+    "patronus": "Stag"},
+    
+    {"name": "Ron",
+    "house": "Gryffindor",
+    "patronus": "Jack Russell terrier"},
+
+    {"name": "Draco",
+    "house": "Gryffindor",
+    "patronus": None},
+]
+
+for student in students:
+    print(student["name"], student["house"], student["patronus"], sep=", ")
