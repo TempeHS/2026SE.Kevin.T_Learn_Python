f91ff8c8c5417f947ff51e8bcdf7df5b78ba83e7
Kevin T <126206698+kevintran120@users.noreply.github.com>
2025-03-27 09:33:43 +0000

    updated coz i forgot something

--- 6-FileIO/lines/lines.py
+++ 6-FileIO/lines/lines.py
@@ -13,6 +13,13 @@ if not sys.argv[1].endswith(".py"):
     print("Not a python file")
     sys.exit()
 
+try:
+    open(sys.argv[1])
+except FileNotFoundError:
+    print("File not found")
+    sys.exit()
+
+
 with open(sys.argv[1]) as file:
     for line in file:
         if line.strip(): #checks if its only spaces
