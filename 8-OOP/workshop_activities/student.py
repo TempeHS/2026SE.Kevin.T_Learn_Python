27c05ef80eb61d1bb5f2b23907263ff6f6b15c86
Kevin T <126206698+kevintran120@users.noreply.github.com>
2025-05-12 11:45:09 +0000

    clear

--- 8-OOP/workshop_activities/student.py
+++ 8-OOP/workshop_activities/student.py
@@ -1,23 +1,21 @@
 class Student:
     def __init__(self, name, house):
-        if not name:
-            raise ValueError("Missing name")
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
         self.name = name
         self.house = house
 
     def __str__(self):
         return f"{self.name} from {self.house}"
+    
+    @classmethod
+    def get(cls):
+        name = input("Name: ")
+        house = input("House: ")
+        return cls(name, house)
 
 def main():
-    student = get_student()
+    student = Student.get()
     print(student)
-
-def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    return Student(name, house)
+    
 
 if __name__ == "__main__":
     main()
